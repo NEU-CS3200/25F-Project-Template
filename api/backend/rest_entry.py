d14f363bf2e67501e410cbe497de262b78b0cffe
natarajan-nandhini <natarajan.nan@northeastern.edu>
2025-12-06 12:54:22 -0500

    Add insert SQL files, remove old schema, update Dockerfile and rest_entry

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -37,19 +37,22 @@ def create_app():
     app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("MYSQL_ROOT_PASSWORD").strip()
     app.config["MYSQL_DATABASE_HOST"] = os.getenv("DB_HOST").strip()
     app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("DB_PORT").strip())
-    app.config["MYSQL_DATABASE_DB"] = os.getenv(
-        "DB_NAME"
-    ).strip()  # Change this to your DB name
+    app.config["MYSQL_DATABASE_DB"] = os.getenv("DB_NAME").strip()  # Change this to your DB name
 
     # Initialize the database object with the settings above.
     app.logger.info("current_app(): starting the database connection")
     db.init_app(app)
+
+    @app.route("/")
+    def welcome():
+        return "<h1>Welcome to the Fall Summer CS 3200 Project Template Repo/<h1>"
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.logger.info("create_app(): registering blueprints with Flask app object.")
     app.register_blueprint(simple_routes)
     app.register_blueprint(ngos, url_prefix="/ngo")
+
 
     # Don't forget to return the app object
     return app
